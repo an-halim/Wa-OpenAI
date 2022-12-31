15055c1bf8e53053172c2f064e9fc90e185746e7
an-halim <halimbla2@gmail.com>
2022-12-31 07:14:32 +0700

    move fe to other branch

--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const auth = require('../controllers/auth');
-const app = require('express');
-const router = app.Router();
-
-router.post('/login', auth.login);
-router.post('/register', auth.register);
-router.post('/verify', auth.verify);
-
-module.exports = router;
